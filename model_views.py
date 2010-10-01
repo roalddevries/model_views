ce9ed6380c7356cd57f1c9bad6899b49649492e9
Roald de Vries <git@roalddevries.nl>
2010-10-01 15:22:58 +0200

    first stub

---
+++ model_views.py
@@ -0,0 +1,10 @@
+class ModelView(object):
+    """
+        Supplies a web interface for a model
+
+        What way to use, 'the right way' or one similar to ModelForm?
+    """
+
+    def __init__(self):
+        super(ModelView, self).__init__()
+
